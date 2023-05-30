12b932f7476c32c8d9c2cb50eb276970859c8fac
Richarley12 <richar.arley12@gmail.com>
2023-05-29 20:47:51 -0500

    se_reservar

--- API.js
+++ API.js
@@ -20,5 +20,4 @@ export class API {
     conectarConBD(){
         establecerConexion()
     }
-
 }
--- models/modeloHabitacion.js
+++ models/modeloHabitacion.js
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
-
 const Habitacion = new Schema({
     nombre:{
         type:String,
         required:true
     },
     foto:{
-        type:[String],
+        type:String,
         required:true
     },
     descripcion:{
@@ -22,8 +21,11 @@ const Habitacion = new Schema({
     numeropersonas:{
         type:Number,
         required:true
+    },
+    estado:{
+        type:Boolean,
+        required:true
     }
-
 })
 
 export const modeloHabitacion=mongoose.model('habitaciones',Habitacion)
